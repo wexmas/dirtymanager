2163ca09ab376724fdb14a7d467f7a32b7a5d131
Vasiliy Baryshnikov <ichristmas21@gmail.com>
2019-03-04 21:01:50 +0300

    tok

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import time
 slack_client = SlackClient("xoxp-345187792039-344274260453-566478079093-7790aebe7b2a15608e718c0fc3a561e5")
 
 if slack_client.rtm_connect(with_team_state=False):
-    print "Successfully connected, listening for events"
+    print ("Successfully connected, listening for events")
     while True:
         print slack_client.rtm_read()
 
