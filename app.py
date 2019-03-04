eaf0f891253b1d12d3b06ffb3fa2c5d7cca99b45
Vasiliy Baryshnikov <ichristmas21@gmail.com>
2019-03-04 20:56:13 +0300

    tok

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from slackclient import SlackClient
 
-slack_client = SlackClient("xoxb-*******************")
+slack_client = SlackClient("xoxp-345187792039-344274260453-566478079093-7790aebe7b2a15608e718c0fc3a561e5")
 
 api_call = slack_client.api_call("users.list")
 if api_call.get('ok'):
