2f6df73d4a15c096f4e19a68be3880100e579f8c
Vasiliy Baryshnikov <ichristmas21@gmail.com>
2019-03-04 20:55:12 +0300

    test2

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-print("Hello world!")
+from slackclient import SlackClient
+
+slack_client = SlackClient("xoxb-*******************")
+
+api_call = slack_client.api_call("users.list")
+if api_call.get('ok'):
+    users = api_call.get('members')
+    for user in users:
+        print user.get('name')
