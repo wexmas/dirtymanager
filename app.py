ea77bbfde184ca3f4345487b9efafdd0cbc1dd1f
Vasiliy Baryshnikov <ichristmas21@gmail.com>
2019-03-04 20:58:16 +0300

    tok

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from slackclient import SlackClient
+import time
 
 slack_client = SlackClient("xoxp-345187792039-344274260453-566478079093-7790aebe7b2a15608e718c0fc3a561e5")
 
-api_call = slack_client.api_call("users.list")
-if api_call.get('ok'):
-    users = api_call.get('members')
-    for user in users:
-        print user.get('name')
+if slack_client.rtm_connect(with_team_state=False):
+    print "Successfully connected, listening for events"
+    while True:
+        print slack_client.rtm_read()
+
+        time.sleep(1)
+else:
+    print "Connection Failed"
